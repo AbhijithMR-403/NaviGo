6a50f16ca110e59274dc6a6dd3808b35219665f4
Abhijith M R <abhijithmr581@gmail.com>
2024-02-17 14:24:28 +0530

    Local Storage issue resolved

--- Frontend/src/components/api/api_instance.js
+++ Frontend/src/components/api/api_instance.js
@@ -2,9 +2,6 @@ import axios from "axios";
 import { API_BASE_URL } from "../../constant/api";
 
 
-const token = localStorage.getItem('access');
-
-console.log(token);
 
 export const AuthAxios = axios.create({
     baseURL: `${API_BASE_URL}/auth`,
@@ -17,7 +14,7 @@ export const AdminAxios = axios.create({
     baseURL: `${API_BASE_URL}/admin`,
     timeout: 3000,
     headers: {
-      'Authorization': `Bearer ${token}`,
+      'Authorization': `Bearer ${localStorage.getItem('access')}`,
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
@@ -27,7 +24,7 @@ export const AdminBusAxios = axios.create({
     baseURL: `${API_BASE_URL}`,
     timeout: 3000,
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${localStorage.getItem('access')}`,
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
