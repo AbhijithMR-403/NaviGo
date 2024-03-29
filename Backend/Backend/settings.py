8661c3397a8f563686aa0f092dbfd30e93976e86
Abhijith M R <abhijithmr581@gmail.com>
2024-03-29 20:35:34 +0530

    increate token lifetime

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
